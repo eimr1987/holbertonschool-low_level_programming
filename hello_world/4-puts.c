dfdb116687131a98359f6c4ff046c61221fb8634
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-20 22:29:28 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,9 +8,6 @@
  */
 int main(void)
 {
-  puts("Programming is like bulding a multilingual puzzle");
-  return (0);
+	puts("Programming is like bulding a multilingual puzzle");
+	xsxreturn (0);
 }
-
-
-       
