541e5bb3fb2ac0bc4f533dac756bf9d94f3b9652
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-20 23:49:38 +0000

    test

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	char d;
 	float f;
 
-	//Imprimer el valor de tgamaño en dos diferentes arquitecturas
+	/*Imprimer el valor de tgamaño en dos diferentes arquitecturas*/
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
