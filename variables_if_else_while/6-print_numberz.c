d3062cf7585eb41a6dbfa489b6c8ba6f63b9263d
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-21 05:54:34 +0000

    test

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - imprime del 0 al 9 utilizando for
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	/*dclaramos la variable bandera*/
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
