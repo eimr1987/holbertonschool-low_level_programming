a4ae3edc4bf58ef2faf7c5c0fbb98f7af482df15
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-21 08:46:26 +0000

    test

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+/**
+ * main - Aqui van los prototipos, algo asi como una biblioteca
+ *void prototypes (void)
+ *int prototypes (int)
+ *
+ */
+void print_alphabet_x10(void);
+void print_alphabet(void);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+int _putchar(char);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
