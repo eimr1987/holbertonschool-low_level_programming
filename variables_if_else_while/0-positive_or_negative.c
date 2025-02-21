c6fbc8c3e299cfd7eba7c5dfd57b12a7550c9d14
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-21 03:46:09 +0000

    test

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Funcion random positive-negative
+ *
+ * Return: 0
+ *
+ */
 int main(void)
 {
 	int n;
