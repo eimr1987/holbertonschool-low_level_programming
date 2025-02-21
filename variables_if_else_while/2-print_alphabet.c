dc9e5e41b185a7158f1f6668e2073057a40e717b
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-21 04:55:18 +0000

    letras del mal

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - imprime de la a a la z en minusculas
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char letra = 'a';
+
+	while(letra <= 'z')
+	{
+		putchar(letra);
+		letra++;
+	}
+	putchar('\n');
+	return (0);
+}
