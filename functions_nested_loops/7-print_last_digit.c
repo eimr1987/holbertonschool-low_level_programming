1a235cbdc5ff9b4023ea8ad2c15fecde165088a4
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-23 04:55:44 +0000

    test

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit(int) - imprimir el últino digito de un número
+ *
+ * @n: jijiji
+ *
+ * Return: ultimo digito
+ *
+ */
+int print_last_digit(int n)
+{
+	ultimo_digito = n % 10;
+
+	return (ultimo_digito);
+}
