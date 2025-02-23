34141a1562206e877d71c99a163885232595daf5
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-23 10:09:10 +0000

    test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,17 @@ void  print_to_98(int n)
 	int i;
 
 	if (n == 98)
-		printf("%d", n);
-
-	else if (n < 98)
+		printf("%d\n", n);
+	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
+		printf("%d\n", n);
 	}
         else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 			printf("%d, ", i);
+		printf("%d\n", n);
 	}
 }
