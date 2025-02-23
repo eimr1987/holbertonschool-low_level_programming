7cacc46de3e3c043b3b0fb11837457249ea97808
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-23 08:21:54 +0000

    test

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - suma dos valores por parametros enteros
+ *
+ * @x: valor 1
+ * @y: valor 2
+ * @z: resultado
+ */
+int add(int x, int y)
+{
+	int z, x, y;
+
+	z = x + y;
+
+	return(z);
+}
