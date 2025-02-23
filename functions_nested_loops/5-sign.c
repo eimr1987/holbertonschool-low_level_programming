08b3ef4c8362f762bacdc852c3afd3e071c6eab9
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-23 01:09:14 +0000

    test

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,20 +12,19 @@
  */
 int print_sign(int n)
 {
-	/*validamos el número*/
-	if (n > 0)
+	if (n > 0) //si es mayor que cero
 	{
-		_putchar('+');
+		_putchar('+');//colocamos signo
 		return (1); /*es positivo*/
 	}
-	else if (n == 0)
+	else if (n == 0) //si es igual a cero
 	{
-		_putchar('0');
+		_putchar('0'); //imprimos un cero
 		return (0); /*es igual a cero*/
 	}
-	else
+	else //ninguno de los anteriores
 	{
-		_putchar('-');
+		_putchar('-');//es negativo
 		return (-1); /*es negativo*/
 	}
 }
