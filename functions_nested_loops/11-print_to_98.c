25e67bd11785d8f27fe40849f4a24a5d0bb58c26
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-23 09:57:20 +0000

    test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,30 +9,21 @@
  * Return: impresiones
  *
  */
-void print_to_98(int n)
+int print_to_98(int n)
 {
 	int i;
 
 	if (n == 98)
-	{
 		printf("%d", n);
-		return;
-	}
 
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
-		{
 			printf("%d, ", i);
-		}
-		return;
 	}
         if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
-		{
 			printf("%d, ", i);
-		}
-	        return;
 	}
 }
