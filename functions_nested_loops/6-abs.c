47b2442b68414eac573668a6aeab3a94fde60812
erick mendez <eivan.mrmirez@protonmail.com>
2025-02-23 04:37:07 +0000

    test

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * _abs - valor absoluto
+ *
+ * @n: Wiiiiii
+ *
+ * Return: valor absoluto de un entero
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
