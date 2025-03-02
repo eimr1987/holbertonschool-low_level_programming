87b0d741f8060fd32ce37b57ddf530accd800fb7
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-02 05:09:45 +0000

    test

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - funcion que multiplica dos valores
+ * @a: valor a
+ * @b: segundo dato
+ * Return: numero entero con el resultado del producto
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
