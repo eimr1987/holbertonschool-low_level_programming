92768d0d37c7b9846b2f62af2faee425e2a8f442
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-02 07:09:30 +0000

    test

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - imprime diagonales
+ *
+ * @n: validacion
+ *
+ * Return: cero
+ */
+void print_diagonal(int n)
+{
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\');
+		}
+	}
+	_putchar('\n');
+}
