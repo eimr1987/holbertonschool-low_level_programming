fac433982e815148b1124860dc34082978357d96
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-02 05:54:52 +0000

    test

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_most_numbers - imprimer del 0 al 9, ignorar 2 y 4;
+ *
+ * Return: cero
+ */
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		if (i != '2' || i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
