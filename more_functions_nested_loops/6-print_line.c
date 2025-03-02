b4dba3beecc6a2871a7d92e6db658771d55d64ed
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-02 06:44:09 +0000

    test

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - imprime _, dependiente dl número enviado
+ *
+ * @n: validacion
+ *
+ * Return: cero
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	}
+}
