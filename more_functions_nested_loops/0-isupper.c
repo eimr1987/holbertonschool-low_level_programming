bd4f8b7e61eed3a1f9973b703d360b05982ec29e
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-02 04:27:44 +0000

    Write a function that checks for uppercase character.

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * _isupper - Write a function that checks for uppercase character.
+ * @c: variable a validar
+ * Return: 1 si es mayuscula; 0 si no lo es
+ */
+int _isupper(int c)
+{
+	if  ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#DEFINE MAIN_H
+
+/**
+ * main - Header de los ejercicios
+ * void
+ * int
+ */
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+int _putchar(char);
+
+#endif
