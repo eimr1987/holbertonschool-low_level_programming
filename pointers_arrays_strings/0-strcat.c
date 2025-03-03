13cb9ab4d01fcb27f442d46df9de7fecd56dc154
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-03 02:57:41 +0000

    test

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatena dos strings
+ * @dest: destino
+ * @src: origen
+ * Return: concatenacion
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+	int recorrido_en_dest = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		recorrido_en_dest++;
+	}
+	for (j = 0; scr[j] != '\0'; j++)
+	{
+		dest[recorrido_en_dest] = src[j];
+		recorrido_en_dest++;
+		j++;
+	}
+	dest[recorrido_en_dest] = '\0';
+
+	return (dest);
+}
