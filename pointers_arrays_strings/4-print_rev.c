a61073ecba4a0f9538a510a4b300cc039cdf000d
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-03 00:31:10 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 {
 	int i, longitud_cadena;
 
-	longitud_cadena = _strlen(*s);
+	longitud_cadena = _strlen(s);
 	for (i = longitud_cadena; i >= 0; i--)
 	{
 		_putchar(s[i]);
