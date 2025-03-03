fe73eeb16a81dca7ebe04c636a8efcbf260ab5d0
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-03 00:45:19 +0000

    test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Imprime una adena en reversa
+ * @s: validacion
+ * Return: cero
+ */
+void print_rev(char *s)
+{
+	int i, longitud_cadena = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		longitud_cadena++;
+	}
+	for (i = longitud_cadena - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
