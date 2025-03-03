6eb22fec1a4c2ff3c1ac421647f71699520c8719
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-03 01:03:05 +0000

    test

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,14 @@
 void rev_string(char *s)
 {
 	int i, longitud_cadena = 0;
-
+	char tmp;
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		longitud_cadena++;
 	}
 
-	char tmp;
-
-	for (i = longitud_cadena - 1; i >= longitd_cadena / 2; i--)
+	for (i = longitud_cadena - 1; i >= longitud_cadena / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[longitud_cadena - i - 1];
