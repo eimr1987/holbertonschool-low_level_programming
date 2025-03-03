0bcf7e58d414869349178b7b755e3832285dbec9
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-03 03:03:17 +0000

    test

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src)
 	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[recorrido_en_dest] = src[j];
+		dest[recorrido_en_dest + 1] = src[j];
 		recorrido_en_dest++;
 		j++;
 	}
