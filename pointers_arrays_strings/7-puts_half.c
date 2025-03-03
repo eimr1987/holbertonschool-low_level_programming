d2f6fdb75dab759321ca4076da9559d41e4dc5dc
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-03 01:28:06 +0000

    test

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - Imprime la mitad de una cadena
+ * @str: validacion
+ * Return: cero
+ */
+void puts_half(char *str)
+{
+	int i, longitud_cadena = 0, n = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		longitud_cadena++;
+	}
+	n = longitud_cadena / 2;
+
+	for (i = n; i < longitud_cadena; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
