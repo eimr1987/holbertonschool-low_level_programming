cdf559738e2f349f01282857a54757aae08f1c01
erick mendez <eivan.mrmirez@protonmail.com>
2025-03-24 00:06:16 +0000

    test

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,13 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+
 typedef struct printer
 {
 	char *symbol;
